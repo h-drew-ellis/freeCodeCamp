886acd613b58fcaa80f0bd055fdb3876893b6706
Andrew Ellis <h.drew.ellis@gmail.com>
2020-08-20 01:11:16 -0500

    cleanup

--- .history/JavaScript/BasicJavaScript/6MadLib_20200819161718.js
+++
@@ -1,5 +0,0 @@
-//Lesson 34
-//Word Blanks
-"My" + myAdjective + " brown " + myNoun + " Bob, " + myVerb + " home " + myAdverb + ".";
-// When concatenating strings as above, do ensure you had spaces between your variables or add an empty space: " ", as when it parses it doenst add the space for you.
-
--- .history/JavaScript/BasicJavaScript/Comments_20200727150656.js
+++
@@ -1,7 +0,0 @@
-//Lesson 1
-//Basic JavaScript: Comment Your JavaScript Code
-
-//Comment type 1
-
-/* Comment Example 
-2*/
